63dd1ad22f20c6974607069732e6983fca2f585c
Amon Chowdhury <amonchowdhury8@gmail.com>
2026-01-29 23:58:18 +0600

    Remove debug banner from MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -33,11 +33,16 @@ class MyApp extends StatelessWidget {
   final ChatAnalysisResponse testData;
   final bool onboardingComplete;
 
-  const MyApp({super.key, required this.testData, required this.onboardingComplete});
+  const MyApp({
+    super.key,
+    required this.testData,
+    required this.onboardingComplete,
+  });
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
